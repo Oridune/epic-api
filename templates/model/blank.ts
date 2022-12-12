c0cf5021a86d52f5f9418bd166f7d70d92b866de
Saif Ali Khan <saffellikhan@gmail.com>
2022-12-12 19:01:42 +0500

    Minor updates

--- templates/model/blank.ts
+++ templates/model/blank.ts
@@ -5,7 +5,9 @@ export interface I$_NameModel {
   updatedAt: Date;
 }
 
-export const $_NameModel = mongoose.model<I$_NameModel>(
-  "$_Name",
-  new mongoose.Schema({}, { timestamps: true, versionKey: false })
+export const $_NameSchema = new mongoose.Schema(
+  {},
+  { timestamps: true, versionKey: false }
 );
+
+export const $_NameModel = mongoose.model<I$_NameModel>("$_Name", $_NameSchema);
