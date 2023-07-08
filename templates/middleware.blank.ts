c27ad4537ff75984eef933f3705bcc480c66449f
Saif Ali Khan <saffellikhan@gmail.com>
2023-07-08 22:50:14 +0500

    Update middleware.blank.ts

--- templates/middleware.blank.ts
+++ templates/middleware.blank.ts
@@ -1,6 +1,7 @@
 import e from "validator";
 import { type RouterContext } from "oak";
 
+// If this is a global middleware, do not add arguments to the factory function.
 export default () =>
   async (ctx: RouterContext<string>, next: () => Promise<unknown>) => {
     // Query Validation (You can remove this validation if not required)
