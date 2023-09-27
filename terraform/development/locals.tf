57a2fd5f97e77b3ea39aff7ad80a2f781aad8095
Saif Ali Khan <saffellikhan@gmail.com>
2023-09-27 23:20:34 +0500

    Terraform added

---
+++ terraform/development/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  dot_env_regex  = "(?m:^\\s*([^#\\s]\\w*)\\s*=\\s*[\"']?(.*[^\"'\\s])[\"']?\\s*$)"
+  global_dot_env = "../../env/.env"
+  local_dot_env  = "../../env/.development.env"
+  dot_env = merge(
+    { for tuple in regexall(local.dot_env_regex, file(local.global_dot_env)) : tuple[0] => sensitive(tuple[1]) },
+    { for tuple in regexall(local.dot_env_regex, file(local.local_dot_env)) : tuple[0] => sensitive(tuple[1]) }
+  )
+}
---
+++ terraform/development/main.tf
@@ -0,0 +1,36 @@
+resource "aws_lightsail_container_service" "epic-api-development" {
+  name  = "epic-api-development"
+  power = "micro"
+  scale = 1
+}
+
+resource "aws_lightsail_container_service_deployment_version" "epic-api-development_deployment" {
+  container {
+    container_name = "epic-api-development"
+    image          = "oridune/epic-api:v1.0.0"
+
+    environment = local.dot_env
+
+    ports = {
+      # Consistent with the port exposed by the Dockerfile
+      3742 = "HTTP"
+    }
+  }
+
+  public_endpoint {
+    container_name = "epic-api-development"
+    # Consistent with the port exposed by the Dockerfile
+    container_port = 3742
+
+    health_check {
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout_seconds     = 2
+      interval_seconds    = 5
+      path                = "/"
+      success_codes       = "200-499"
+    }
+  }
+
+  service_name = aws_lightsail_container_service.epic-api-development.name
+}
