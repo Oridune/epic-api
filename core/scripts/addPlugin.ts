db2f7ff72db944a7320043823e12f2ac10333877
Saif Ali Khan <saffellikhan@gmail.com>
2023-10-05 15:57:05 +0500

    Update addPlugin.ts

--- core/scripts/addPlugin.ts
+++ core/scripts/addPlugin.ts
@@ -188,6 +188,7 @@ export const addPlugin = async (options: {
             for (const EntryName of [
               ".git",
               "core",
+              "docs",
               "env",
               "terraform",
               "database.ts",
