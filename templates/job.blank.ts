c56b8fa828e37f26624a36d2de0a34c514090804
Saif Ali Khan <saffellikhan@gmail.com>
2024-04-28 17:12:44 +0500

    Server startup optimized

--- templates/job.blank.ts
+++ templates/job.blank.ts
@@ -3,6 +3,7 @@ import { Application } from "oak";
 export default {
   pre: (_app: Application) => {
     // Write the logic to execute on app start (before server starts listening)...
+    // Pre job executes before the app is populated with routes
 
     return async () => {
       // This code executes when the app receives "SIGINT" | "SIGTERM" | "SIGBREAK" signal.
@@ -12,6 +13,7 @@ export default {
 
   post: (_app: Application) => {
     // Write the logic to execute on app start (before server starts listening)...
+    // Post job executes after the app is populated with routes
 
     return async () => {
       // This code executes when the app receives "SIGINT" | "SIGTERM" | "SIGBREAK" signal.
