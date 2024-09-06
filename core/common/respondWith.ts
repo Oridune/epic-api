8ce589d781f72acd41a7920fad7080f9324ccb32
Saif Ali Khan <saffellikhan@gmail.com>
2024-09-06 23:13:54 +0500

    Minor updates to serveInspector middleware

--- core/common/respondWith.ts
+++ core/common/respondWith.ts
@@ -7,28 +7,32 @@ export const respondWith = async (
   ctx: Context,
   res: Response | RawResponse,
 ) => {
-  if (res instanceof Response) {
-    res.metrics({
-      respondInMs: Date.now() - ctx.state._requestStartedAt,
-    });
+  if (ctx.respond && ctx.response.writable) {
+    if (res instanceof Response) {
+      res.metrics({
+        respondInMs: Date.now() - ctx.state._requestStartedAt,
+      });
+    }
+
+    // Append headers
+    res.getHeaders().forEach((v, k) => ctx.response.headers.append(k, v));
+
+    // Set status code & body
+    ctx.response.status = res.getStatusCode();
+    ctx.response.body = res.getBody(
+      ["1", undefined].includes(
+          Env.getSync("TRANSLATE_RESPONSE_MESSAGES", true),
+        )
+        ? { translator: ctx.t }
+        : undefined,
+    );
+
+    try {
+      ctx.state._body = await ctx.request.body().value;
+    } catch {
+      // Do nothing...
+    }
+
+    Events.dispatch("response", { detail: { ctx, res } });
   }
-
-  // Append headers
-  res.getHeaders().forEach((v, k) => ctx.response.headers.append(k, v));
-
-  // Set status code & body
-  ctx.response.status = res.getStatusCode();
-  ctx.response.body = res.getBody(
-    ["1", undefined].includes(Env.getSync("TRANSLATE_RESPONSE_MESSAGES", true))
-      ? { translator: ctx.t }
-      : undefined,
-  );
-
-  try {
-    ctx.state._body = await ctx.request.body().value;
-  } catch {
-    // Do nothing...
-  }
-
-  Events.dispatch("response", { detail: { ctx, res } });
 };
--- core/middlewares/serveInspector.ts
+++ core/middlewares/serveInspector.ts
@@ -1,55 +1,59 @@
 // deno-lint-ignore-file no-explicit-any
 import { Context, Status } from "oak";
 
-export const serveInspector =
-  () => async (ctx: Context<Record<string, any>, Record<string, any>>) => {
-    const inspectorHost = "localhost:9229";
+export const serveInspector = () =>
+async (
+  ctx: Context<Record<string, any>, Record<string, any>>,
+  next: () => Promise<unknown>,
+) => {
+  const inspectorHost = "localhost:9229";
 
-    if (ctx.isUpgradable) {
-      const ws = new WebSocket(
-        `ws://${inspectorHost}${ctx.request.url.pathname}`,
-      );
+  if (ctx.isUpgradable) {
+    const ws = new WebSocket(
+      `ws://${inspectorHost}${ctx.request.url.pathname}`,
+    );
 
-      const clientSocket = ctx.upgrade();
-      const messageStack: any[] = [];
+    const clientSocket = ctx.upgrade();
+    const messageStack: any[] = [];
 
+    clientSocket.onmessage = (message) => {
+      messageStack.push(message);
+    };
+
+    // Wait for the WebSocket connection to be open
+    ws.onopen = () => {
       clientSocket.onmessage = (message) => {
-        messageStack.push(message);
-      };
-
-      // Wait for the WebSocket connection to be open
-      ws.onopen = () => {
-        clientSocket.onmessage = (message) => {
-          if (ws.readyState === WebSocket.OPEN) {
-            ws.send(message.data);
-          }
-        };
-
-        ws.onmessage = (message) => {
-          if (clientSocket.readyState === WebSocket.OPEN) {
-            clientSocket.send(message.data);
-          }
-        };
-
-        ws.onclose = () => {
-          if (clientSocket.readyState === WebSocket.OPEN) {
-            clientSocket.close();
-          }
-        };
-
-        for (const msg of messageStack) {
-          ws.send(msg.data);
-        }
-      };
-
-      clientSocket.onclose = () => {
         if (ws.readyState === WebSocket.OPEN) {
-          ws.close();
+          ws.send(message.data);
         }
       };
 
-      ctx.response.status = Status.Accepted;
-    } else {
+      ws.onmessage = (message) => {
+        if (clientSocket.readyState === WebSocket.OPEN) {
+          clientSocket.send(message.data);
+        }
+      };
+
+      ws.onclose = () => {
+        if (clientSocket.readyState === WebSocket.OPEN) {
+          clientSocket.close();
+        }
+      };
+
+      for (const msg of messageStack) {
+        ws.send(msg.data);
+      }
+    };
+
+    clientSocket.onclose = () => {
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.close();
+      }
+    };
+
+    ctx.response.status = Status.Accepted;
+  } else {
+    try {
       const url = new URL(
         ctx.request.url.pathname,
         `http://${inspectorHost}`,
@@ -74,5 +78,8 @@ export const serveInspector =
           "https://deno.land/favicon.ico",
           "https://oridune.com/assets/oridune-8999a42e.svg",
         );
+    } catch {
+      await next();
     }
-  };
+  }
+};
