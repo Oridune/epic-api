9b21d4e76d323f10e5321ce6188f4b57bfeedbcd
Saif Ali Khan <saffellikhan@gmail.com>
2024-10-26 21:11:20 +0500

    Mongo updated

--- core/middlewares/errorHandler.ts
+++ core/middlewares/errorHandler.ts
@@ -8,6 +8,7 @@ import {
   respondWith,
   Response,
 } from "@Core/common/mod.ts";
+import { inspect } from "node:util";
 
 export const errorHandler = () =>
 async (
@@ -51,7 +52,7 @@ async (
     if (!Env.is(EnvType.PRODUCTION)) {
       console.error(
         `${ctx.request.method.toUpperCase()}: ${ctx.request.url.pathname}`,
-        error,
+        inspect(error, true, Infinity, true),
       );
 
       if (error.cause !== undefined) {
