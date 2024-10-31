69a50aa27db6f7ef45373cbdca0dfcdfa4306835
Saif Ali Khan <saffellikhan@gmail.com>
2024-10-31 13:00:49 +0500

    Terraform variables def update

--- terraform/development/variables.tf
+++ terraform/development/variables.tf
@@ -10,6 +10,18 @@ variable "aws_secret_key" {
   type = string
 }
 
+variable "default_container_image" {
+  type = string
+}
+
 variable "container_image" {
+  type = string
+}
+
+variable "image_name" {
+  type = string
+}
+
+variable "image_version" {
   type = string
 }
