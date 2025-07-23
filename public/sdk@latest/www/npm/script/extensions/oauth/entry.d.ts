46c771594a2ee1d26a8e5e46e5997ce289d70e8a
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2025-07-23 22:53:03 +0500

    Generate SDK re-written

--- public/sdk@latest/www/npm/script/extensions/oauth/entry.d.ts
+++
@@ -1,9 +0,0 @@
-/**
-* This SDK extension is used within SDK generation process
-* You cannot import any files from the epic api or any of its sources here cause it will not work.
-* Your code should be isolated in the this/current folder.
-*
-*/
-export declare class oauthEntry {
-}
-//# sourceMappingURL=entry.d.ts.map
