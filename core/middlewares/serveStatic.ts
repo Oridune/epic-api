da574effff72d0de9bcccba0ba740a5eb4268e9b
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2025-07-28 00:55:56 +0500

    Minor fixes

--- core/middlewares/serveStatic.ts
+++ core/middlewares/serveStatic.ts
@@ -1,4 +1,5 @@
 // deno-lint-ignore-file no-explicit-any
+import { join } from "path";
 import { existsSync } from "dfs";
 import { Context, send, Status } from "oak";
 
@@ -12,18 +13,20 @@ export const serveStatic = (prefix: string, root: string) => {
     };
   }
 
-  const Prefix = new RegExp(`^/${prefix}/?`);
-
-  const WWWItems = Deno.readDirSync(root);
-
   let IndexFile = "index.html";
 
-  for (const item of WWWItems) {
-    if (/^index.*/.test(item.name)) {
-      IndexFile = item.name;
-      break;
+  if (!existsSync(join(root, IndexFile))) {
+    const WWWItems = Deno.readDirSync(root);
+
+    for (const item of WWWItems) {
+      if (/^index.*/.test(item.name)) {
+        IndexFile = item.name;
+        break;
+      }
     }
   }
+
+  const Prefix = new RegExp(`^/${prefix}/?`);
 
   return async (
     ctx: Context<Record<string, any>, Record<string, any>>,
@@ -33,16 +36,19 @@ export const serveStatic = (prefix: string, root: string) => {
       const SendOptions = { root, index: IndexFile };
       const FilePath = ctx.request.url.pathname.replace(Prefix, "/");
 
-      await send(ctx, FilePath, SendOptions).catch(() => {
-        const IsNotFound = !["/", IndexFile].includes(FilePath);
+      await send(ctx, FilePath, SendOptions).catch(() =>
+        // Resolve to index for react apps
+        send(ctx, "/", SendOptions).catch(() => {
+          const IsNotFound = !["/", IndexFile].includes(FilePath);
 
-        ctx.throw(
-          IsNotFound ? Status.NotFound : Status.Forbidden,
-          IsNotFound
-            ? `File not found! Invalid path '${FilePath}'.`
-            : undefined,
-        );
-      });
+          ctx.throw(
+            IsNotFound ? Status.NotFound : Status.Forbidden,
+            IsNotFound
+              ? `File not found! Invalid path '${FilePath}'.`
+              : undefined,
+          );
+        })
+      );
     } else await next();
   };
 };
