2bf1c9c3ec552ca4cbf1b53545d57b6502e91929
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2025-11-19 19:30:46 +0500

    allow import

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 # Check if INSPECTOR_ENABLED environment variable is set to 'true'
 if [ "$INSPECTOR_ENABLED" = "true" ]; then
     echo "Running deno with inspector enabled."
-    exec deno run --inspect  --unstable-raw-imports --allow-read --allow-write --allow-env --allow-net --allow-sys --allow-run --allow-ffi ./serve.ts
+    exec deno run --inspect  --unstable-raw-imports --allow-read --allow-write --allow-env --allow-net --allow-sys --allow-run --allow-ffi --allow-import ./serve.ts
 else
     echo "Running deno with inspector disabled."
-    exec deno run  --unstable-raw-imports --allow-read --allow-write --allow-env --allow-net --allow-sys --allow-run --allow-ffi ./serve.ts
+    exec deno run  --unstable-raw-imports --allow-read --allow-write --allow-env --allow-net --allow-sys --allow-run --allow-ffi --allow-import ./serve.ts
 fi
