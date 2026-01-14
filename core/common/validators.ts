4cc0958b4e172175ca5e93c3f77d54ab7c3706e5
Saif Ali Khan <saffellikhan@gmail.com>
2026-01-14 21:05:30 +0500

    Deps updated

--- core/common/validators.ts
+++ core/common/validators.ts
@@ -196,44 +196,94 @@ export const testFilters = async <T extends Record<string, unknown>>(
   return false;
 };
 
-export const queryValidator = () =>
-  e.deepCast(e.object(
-    {
-      search: e.optional(e.string()).describe("Enter your search term"),
-      filters: e.optional(filtersSchema),
-      range: e.optional(
-        e.tuple([e.date(), e.date()]),
-      ).describe(
-        "Provide a date range as an array of date object. E.g: [date1, date2]",
-      ),
-      offset: e.optional(e.number().min(0)).default(0),
-      limit: e.optional(e.number().max(2000)).default(2000),
-      sort: e.optional(
-        e.record(e.number().min(-1).max(1)),
-      ).default({ _id: -1 }).describe(
-        "Provide a sorting information in mongodb sort object format",
-      ),
-      project: e.optional(
-        e.record(e.number().min(0).max(1)),
-      ).describe(
-        "Provide a projection information in mongodb project object format",
-      ),
-      includeTotalCount: e.optional(
-        e.boolean()
-          .describe(
-            "If `true` is passed, the system will return a total items count for pagination purpose.",
-          ),
-      ),
-    },
-    { allowUnexpectedProps: true },
-  ));
+export const _queryValidator = e.deepCast(e.object(
+  {
+    search: e.optional(e.string()).describe("Enter your search term"),
+    filters: e.optional(filtersSchema),
+    range: e.optional(
+      e.tuple([e.date(), e.date()]),
+    ).describe(
+      "Provide a date range as an array of date object. E.g: [date1, date2]",
+    ),
+    offset: e.optional(e.number().min(0)).default(0),
+    limit: e.optional(e.number().max(500)).default(100), // Reduced for stability
+    sort: e.optional(
+      e.record(e.number().min(-1).max(1)),
+    ).default({ _id: -1 }).describe(
+      "Provide a sorting information in mongodb sort object format",
+    ),
+    project: e.optional(
+      e.record(e.number().min(0).max(1)),
+    ).describe(
+      "Provide a projection information in mongodb project object format",
+    ),
+    includeTotalCount: e.optional(
+      e.boolean()
+        .describe(
+          "If `true` is passed, the system will return a total items count for pagination purpose.",
+        ),
+    ),
+  },
+  { allowUnexpectedProps: true },
+));
+
+export const queryValidator = () => _queryValidator;
+
+export const _responseValidatorFalse = e.object({
+  status: e.false(),
+  messages: e.optional(e.array(
+    e.partial(
+      e.object({
+        message: e.string(),
+        location: e.string(),
+        name: e.string(),
+      }).rest(e.any()),
+    ),
+  )),
+  metrics: e.optional(
+    e.partial(
+      e.object({
+        handledInMs: e.number(),
+        respondInMs: e.number(),
+      }).rest(e.any()),
+    ),
+  ),
+  metadata: e.optional(e.record(e.any())),
+});
+
+export const _responseValidatorTrue = e.object({
+  status: e.true(),
+  messages: e.optional(e.array(
+    e.partial(
+      e.object({
+        message: e.string(),
+        location: e.string(),
+        name: e.string(),
+      }).rest(e.any()),
+    ),
+  )),
+  metrics: e.optional(
+    e.partial(
+      e.object({
+        handledInMs: e.number(),
+        respondInMs: e.number(),
+      }).rest(e.any()),
+    ),
+  ),
+  metadata: e.optional(e.record(e.any())),
+});
 
 export const responseValidator = <T extends (BaseValidator)>(data?: T, opts?: {
   falseResponse?: boolean;
-}) =>
-  e.object({
+}) => {
+  if (!data) {
+    if (opts?.falseResponse) return _responseValidatorFalse;
+    else return _responseValidatorTrue;
+  }
+
+  return e.object({
     status: opts?.falseResponse ? e.false() : e.true(),
-    ...(data ? { data } : {}),
+    data,
     messages: e.optional(e.array(
       e.partial(
         e.object({
@@ -253,6 +303,7 @@ export const responseValidator = <T extends (BaseValidator)>(data?: T, opts?: {
     ),
     metadata: e.optional(e.record(e.any())),
   });
+};
 
 export const normalizeFilterExpression = (
   value?: string | number | boolean | inferOutput<typeof valueSchema>,
